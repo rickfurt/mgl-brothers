dc4f9c066389a66823a515167cd7dcd1ee01126f
rickfurt <ricardofurtado2609@gmail.com>
2020-05-01 10:05:28 +1000

    fix(structure): create a node server to comply with heroku requirements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 var path = require('path');
 
 express.static('public')
@@ -14,4 +14,4 @@ var public = __dirname + '/public';
 app.get("/", function (req, res) {
     res.sendFile(path.join(public, "/home.html"));
   });
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
